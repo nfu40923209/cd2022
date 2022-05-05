03e814f2081111d3427c3744191bd88b5dd1dfd3
Lu8658813 <40923209@gm.nfu.edu.tw>
2022-05-05 12:04:04 +0800

    w10_nx_integ

---
+++ downloads/w10_nx_integ.js
@@ -0,0 +1,14 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "135": [
+      { type: "gotoframe", rect: { x: 728, y: 505, width: 60, height: 24 }, target: 136 },
+    ],
+    "203": [
+      { type: "gotoframe", rect: { x: 515, y: 306, width: 60, height: 24 }, target: 204 },
+    ],
+  },
+};
